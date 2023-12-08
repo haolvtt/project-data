8f9ca618b05988346d5ccefe0272252e5114374a
Ha-Hieu-Thanh <hieuthanh4a2@gmail.com>
2023-12-09 00:30:05 +0700

    test connect elastic search -> not done

--- My-README.md
+++ My-README.md
@@ -28,7 +28,7 @@ if [ ! -z "${SPARK_APPLICATION_JAR_LOCATION}" ]; then
     echo "Submit application ${SPARK_APPLICATION_JAR_LOCATION} with main class ${SPARK_APPLICATION_MAIN_CLASS} to Spark master ${SPARK_MASTER_URL}"
     echo "Passing arguments ${SPARK_APPLICATION_ARGS}"
     /${SPARK_HOME}/bin/spark-submit \
-        --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.0 \
+        --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.0,org.elasticsearch:elasticsearch-hadoop:7.10.0 \
         --class ${SPARK_APPLICATION_MAIN_CLASS} \
         --master ${SPARK_MASTER_URL} \
         --conf "spark.driver.extraJavaOptions=-Dkafka.key.deserializer=org.apache.kafka.common.serialization.StringDeserializer" \
@@ -40,7 +40,7 @@ else
         echo "Submit application ${SPARK_APPLICATION_PYTHON_LOCATION} to Spark master ${SPARK_MASTER_URL}"
         echo "Passing arguments ${SPARK_APPLICATION_ARGS}"
         PYSPARK_PYTHON=python3  /spark/bin/spark-submit \
-            --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.0 \
+            --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.0,org.elasticsearch:elasticsearch-hadoop:7.10.0 \
             --master ${SPARK_MASTER_URL} \
             --conf "spark.driver.extraJavaOptions=-Dkafka.key.deserializer=org.apache.kafka.common.serialization.StringDeserializer" \
             --conf "spark.executor.extraJavaOptions=-Dkafka.key.deserializer=org.apache.kafka.common.serialization.StringDeserializer" \
--- app/app.py
+++ app/app.py
@@ -1,38 +1,130 @@
+# from pyspark.sql import SparkSession
+# from pyspark.sql.functions import from_json, col, explode
+# from pyspark.sql.types import StringType, StructType, StructField, IntegerType, ArrayType
+
+# if __name__ == "__main__":
+#     # Khởi tạo SparkSession
+#     spark = SparkSession.builder.appName("KafkaToHDFS").getOrCreate()
+#     spark.sparkContext.setLogLevel("ERROR")
+
+#     # Định nghĩa schema cho dữ liệu JSON
+#     json_schema = ArrayType(StructType([
+#         StructField("time", StringType(), True),
+#         StructField("open", IntegerType(), True),
+#         StructField("high", IntegerType(), True),
+#         StructField("low", IntegerType(), True),
+#         StructField("close", IntegerType(), True),
+#         StructField("volume", IntegerType(), True),
+#         StructField("ticker", StringType(), True)
+#     ]))
+
+#     # Định nghĩa tham số Kafka
+#     kafka_params = {
+#         "kafka.bootstrap.servers": "kafka:9092",
+#         "subscribe": "thanh-test",
+#         "startingOffsets": "latest"
+#     }
+
+#     # Đọc dữ liệu từ Kafka
+#     kafka_df = spark.readStream.format("kafka").options(**kafka_params).load()
+
+#     # Chuyển đổi cột 'value' từ dạng binary sang chuỗi JSON
+#     kafka_df = kafka_df.selectExpr("CAST(value AS STRING)")
+
+#     # Phân tích cú pháp chuỗi JSON và chuyển đổi thành DataFrame
+#     stock_df = kafka_df.select(from_json(col("value"), json_schema).alias("data"))
+
+#     # Sử dụng hàm explode để biến đổi mảng thành các hàng
+#     stock_df = stock_df.select(explode(col("data")).alias("stock_data")).select("stock_data.*")
+
+#     # Định nghĩa đường dẫn xuất HDFS
+#     output_path = "hdfs://namenode:8020/user/root/kafka_data"
+
+#     # Chỉ định vị trí checkpoint
+#     checkpoint_location = "hdfs://namenode:8020/user/root/checkpoints"
+
+#     # Ghi dữ liệu vào HDFS dưới dạng file parquet
+#     query = stock_df.writeStream \
+#         .outputMode("append") \
+#         .format("json") \
+#         .option("path", output_path) \
+#         .option("checkpointLocation", checkpoint_location) \
+#         .start()
+
+#     query.awaitTermination()
+
+### elastic chua chay dc 
+
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import expr
+from pyspark.sql.functions import from_json, col, explode
+from pyspark.sql.types import StringType, StructType, StructField, IntegerType, ArrayType
 
 if __name__ == "__main__":
-    # Turn off Spark logs
-    spark = SparkSession.builder.appName("KafkaSparkDemo").getOrCreate()
+    # Khởi tạo SparkSession với cấu hình cho Elasticsearch
+    spark = SparkSession.builder.appName("KafkaToHDFSAndElasticsearch") \
+        .config("spark.es.nodes", "elasticsearch") \
+        .config("spark.es.port", "9200") \
+        .config("spark.es.index.auto.create", "true") \
+        .getOrCreate()
     spark.sparkContext.setLogLevel("ERROR")
 
-    # Define Kafka parameters
+    # Định nghĩa schema cho dữ liệu JSON
+    json_schema = ArrayType(StructType([
+        StructField("time", StringType(), True),
+        StructField("open", IntegerType(), True),
+        StructField("high", IntegerType(), True),
+        StructField("low", IntegerType(), True),
+        StructField("close", IntegerType(), True),
+        StructField("volume", IntegerType(), True),
+        StructField("ticker", StringType(), True)
+    ]))
+
+    # Định nghĩa tham số Kafka
     kafka_params = {
         "kafka.bootstrap.servers": "kafka:9092",
         "subscribe": "thanh-test",
-        "startingOffsets": "latest",
-        "key.deserializer": "org.apache.kafka.common.serialization.StringDeserializer",
-        "value.deserializer": "org.apache.kafka.common.serialization.StringDeserializer",
+        "startingOffsets": "latest"
     }
 
-    # Read data from Kafka as a streaming DataFrame
-    kafka_stream_df = spark.readStream.format("kafka").options(**kafka_params).load()
+    # Đọc dữ liệu từ Kafka
+    kafka_df = spark.readStream.format("kafka").options(**kafka_params).load()
 
-    # Explicitly cast the 'value' column to STRING
-    kafka_stream_df = kafka_stream_df.withColumn("value", expr("CAST(value AS STRING)"))
+    # Chuyển đổi cột 'value' từ dạng binary sang chuỗi JSON
+    kafka_df = kafka_df.selectExpr("CAST(value AS STRING)")
 
-    # Define the HDFS output path
+    # Phân tích cú pháp chuỗi JSON và chuyển đổi thành DataFrame
+    stock_df = kafka_df.select(from_json(col("value"), json_schema).alias("data"))
+
+    # Sử dụng hàm explode để biến đổi mảng thành các hàng
+    stock_df = stock_df.select(explode(col("data")).alias("stock_data")).select("stock_data.*")
+
+    # Định nghĩa đường dẫn xuất HDFS
     output_path = "hdfs://namenode:8020/user/root/kafka_data"
 
-    # Specify checkpoint location
+    # Chỉ định vị trí checkpoint
     checkpoint_location = "hdfs://namenode:8020/user/root/checkpoints"
 
-    # Write the streaming DataFrame to HDFS in JSON format, append to an existing file
-    query = kafka_stream_df.writeStream \
+    # Ghi dữ liệu vào HDFS dưới dạng file parquet
+    hdfs_query = stock_df.writeStream \
         .outputMode("append") \
         .format("json") \
         .option("path", output_path) \
         .option("checkpointLocation", checkpoint_location) \
         .start()
 
-    query.awaitTermination()
+    # Ghi dữ liệu vào Elasticsearch
+    es_write_conf = {
+        "es.resource": "index/type",  # Thay thế 'index/type' bằng tên index và type của bạn trong Elasticsearch
+        "es.nodes": "elasticsearch",  # Thay thế 'elasticsearch-node' bằng tên hoặc IP của node Elasticsearch của bạn
+        "es.port": "9200"
+    }
+
+    es_query = stock_df.writeStream \
+        .outputMode("append") \
+        .format("org.elasticsearch.spark.sql") \
+        .options(**es_write_conf) \
+        .start()
+
+    # Chờ đợi cả hai quá trình ghi hoàn tất
+    hdfs_query.awaitTermination()
+    es_query.awaitTermination()
